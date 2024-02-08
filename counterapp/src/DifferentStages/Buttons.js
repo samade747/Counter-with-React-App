6159672f1c98d5a3dac6442aa2023483e4eb3bff
samade747 <samad.e747@gmail.com>
2024-02-08 20:17:07 +0500

    816pm 8 2 2024

---
+++ counterapp/src/DifferentStages/Buttons.js
@@ -0,0 +1,21 @@
+export default function Buttons({ setLevel }){
+
+console.log(setLevel => "smads")
+const previous = () => {
+
+}
+
+const next = () => {
+    
+}
+
+
+return (
+    <div style={{display: "flex", justifyContent: "center"}}>
+        <button onClick={previous}>Previous</button>
+        <button onClick={next}>next</button>
+
+    </div>
+)
+
+}
--- counterapp/src/DifferentStages/DifferentStages.js
+++ counterapp/src/DifferentStages/DifferentStages.js
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import Stages from "./Stages.js";
 import Data from "./Data.js";
-
+import Buttons from "./Buttons.js";
 
 
 export default function DifferentStages(){
+
+    const [level, setLevel] = useState(0)
+ 
 
 return(
     <div>
        <Stages /> 
        <Data />
+       <Buttons setLevel={setLevel} />
     </div>
 )
 
